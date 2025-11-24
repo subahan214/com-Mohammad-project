60c7d63007f0bd1eec65b9ef0f73fe04fd3814b2
Ubuntu <ubuntu@ip-172-31-28-143.ec2.internal>
2025-11-24 15:15:22 +0000

    Convert Spring Boot app to WAR for Tomcat deployment

--- src/main/java/war/App.java
+++ src/main/java/war/App.java
@@ -2,9 +2,16 @@ package war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(proxyBeanMethods = false)
-public class App {
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
